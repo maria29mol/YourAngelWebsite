8e4501248232dc4b0e76adbd2aec2f6da66fb5f0
maria29mol <ms.molfea@gmail.com>
2025-08-06 11:53:40 +0200

    Fix Duplicate Imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
 import Hero from './components/Hero';
 import Features from './components/Features';
 import Stats from './components/Stats';
 import CTA from './components/CTA';
 import Footer from './components/Footer';
 import CookieConsent from './components/CookieConsent';
-import SafeSecure from './components/SafeSecure';
-import AIExpert from './components/AIExpert';
-import TeenFocused from './components/TeenFocused';
 import SafeSecure from './components/SafeSecure';
 import AIExpert from './components/AIExpert';
 import TeenFocused from './components/TeenFocused';
